a3dd83fd0d43d9fbe03fd2865559daa01387fec2
Stefan Frijters <sfrijters@gmail.com>
2024-02-06 13:21:33 +0100

    Extend logwatch-nixos-upgrade script

--- packages/logwatch-scripts/nixos-upgrade.nix
+++ packages/logwatch-scripts/nixos-upgrade.nix
@@ -4,17 +4,25 @@ pkgs.writeShellApplication {
   name = "logwatch-nixos-upgrade";
   text = ''
     package_update_regex="^  */nix/store"
-    keep_output_regex="updating lock file|updated input|github:NixOS/nixpkgs|will be built|will be fetched|^  /nix/store|switching to system configuration|NixOS Upgrade.|error: builder for"
+    keep_output_regex="updating lock file|will be built|will be fetched|^  /nix/store|switching to system configuration|NixOS Upgrade.|error: builder for"
+    updated_output_regex="Updated input"
     error_regex="error: builder for|Failed to start"
 
+    nprint=0
     last=
 
     while IFS= read -r line; do
-        if [[ "''${line}" =~ ''${package_update_regex} ]]; then
+        if [ "''${nprint}" -gt 0 ]; then
+            echo "''${line}"
+            nprint=$((nprint - 1))
+        elif [[ "''${line}" =~ ''${package_update_regex} ]]; then
             # shellcheck disable=SC2001
             package_name="$(echo "''${line}" | sed 's| */nix/store/[^\-]*-||')"
             printf "%s " "''${package_name}"
             last=package
+        elif [[ "''${line}" =~ ''${updated_output_regex} ]]; then
+            echo "''${line}"
+            nprint=2
         elif [[ "''${line}" =~ ''${keep_output_regex} ]]; then
             if [ "''${last}" = package ]; then
                 printf "\n"
